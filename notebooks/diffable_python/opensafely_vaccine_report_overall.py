67ca1f09032e830ddbe78b0fda379b3ec85375b9
HelenCEBM <helencebm@gmail.com>
2021-07-22 11:13:45 +0100

    Main report updates
    Include younger age groups in main report, add items in contents, extra explanatory text

--- notebooks/diffable_python/opensafely_vaccine_report_overall.py
+++ notebooks/diffable_python/opensafely_vaccine_report_overall.py
@@ -1,5 +1,6 @@
 # ---
 # jupyter:
+#   anaconda-cloud: {}
 #   jupytext:
 #     cell_metadata_filter: all
 #     notebook_metadata_filter: all,-language_info
@@ -15,12 +16,14 @@
 # ---
 
 # # OpenSAFELY COVID Vaccine coverage report
-
+#
 # OpenSAFELY is a new secure analytics platform for electronic patient records built on behalf of NHS England to deliver urgent academic and operational research during the pandemic. 
 #
 # This is our regular weekly report on COVID-19 vaccination coverage in England using data from 40% of general practices that use TPP electronic health record software. **The data requires careful interpretation and there are a number of caveats. Please read the full detail about our methods and discussion of our earlier results (as of January 13th) in our preprint paper available [here](https://www.medrxiv.org/content/10.1101/2021.01.25.21250356v2).** 
 #
 # The full analytical methods behind the latest results in this report are available [here](https://github.com/opensafely/nhs-covid-vaccination-uptake).
+#
+#
 
 # +
 from datetime import datetime
@@ -39,7 +42,6 @@ with open(os.path.join("..", "interim-outputs","text", "latest_date.txt"), 'r') 
 
 with open(os.path.join("..", "interim-outputs","text", "latest_date_of_first_dose_for_due_second_doses.txt"), 'r') as file:
     latest_date_13w_fmt = file.read()
-
 # -
 
 # #### 
@@ -54,13 +56,20 @@ with open(os.path.join("..", "interim-outputs","text", "latest_date_of_first_dos
 #  - <a href=#charts60>60-64 population</a>
 #  - <a href=#charts55>55-59 population</a>
 #  - <a href=#charts50>50-54 population</a>
+#  - <a href=#charts40>40-49 population</a>
+# </br>
+# </br>
 # - **<a href=#tables>Current vaccination coverage of each eligible population group, according to demographic/clinical features</a>**
-#   - Includes each of the groups above, plus care home (65+) and LD (aged 16-64) populations.
-#
+#   - Includes each of the groups above, plus <a href=#Cumulative-vaccination-figures-among-care-home-population>care home (65+)</a> and <a href=#Cumulative-vaccination-figures-among-Learning-Disabilities-(aged-16-64)-population>LD (aged 16-64)</a> populations.
+#   - **NEW** - tables now include <a href=#Cumulative-vaccination-figures-among-30-39-population>30-39</a> and <a href=#Cumulative-vaccination-figures-among-18-29-population>18-29</a> populations
+# </br>
+# </br>
 # - Appendix: <a href=#ethnicity>Proportion of each population group for whom ethnicity is known</a>
+#
 
 # # 
 # ## Overview of Vaccination Figures to date <a name='summary' />
+#
 
 # +
 import json
@@ -77,14 +86,25 @@ display(Markdown(f"##### \n"
                 "##### \n" ))
 
 display(Markdown(f"### Vaccine types" ),
-       Markdown("Note: numbers may not sum to 100% as it is not always possible to determine vaccine type given"))
+       Markdown("**Note:** numbers may not sum to 100% as it is not always possible to determine vaccine type given, and patients occasionally have more than one brand recorded on the same day."))
 
 for x in additional_stats.index[0:3]:  
     display(Markdown(f"{x}: {additional_stats.loc[x][0]}\n"))
     
 display(Markdown(f"### Second doses and dose combinations" ))
-for x in additional_stats.index[3:]:  
+display(Markdown("**Note:** second dose figures are raw proportions and do not take into account how many are due, and the proportion due is likely to vary substantially by brand.</br>\
+                For more detailed analysis please refer to our [second dose report](https://reports.opensafely.org/reports/vaccine-coverage-second-doses/)"))
+
+for x in additional_stats.index[3:7]:  
     display(Markdown(f"{x}: {additional_stats.loc[x][0]}\n"))
+    
+display(Markdown("</br>**Note:** mixed doses counts patients with two doses at least 19 days apart, \
+                  excluding patients with two different brands recorded on different days \
+                  or where the first dose was recorded before the given brand was available"))
+       
+for x in additional_stats.index[7:]:  
+    display(Markdown(f"{x}: {additional_stats.loc[x][0]}\n"))
+
     
 
 
@@ -96,6 +116,7 @@ display(Markdown(f"##### \n"
                 codelist and excludes people who are shielding.\n"
                  "\n- Patients are counted in their highest risk category only; e.g. a 65-year-old who is shielding \
                 is only counted in the shielding group, not in the 65-69 population\n"))
+
 # -
 
 # # 
@@ -111,11 +132,12 @@ from image_formats import pick_image_format
 IMAGE_FORMAT = pick_image_format()
 
 show_chart(f"Cumulative vaccination figures.{IMAGE_FORMAT.extension}", IMAGE_FORMAT, title="off")
+
 # -
 
-# ## Trends in vaccination rates of 80+ population according to demographic/clinical features, cumulatively by day. <a name='charts80' />
+# # 
+# ## Trends in vaccination rates of **80+** population according to demographic/clinical features, cumulatively by day. <a name='charts80' />
 # **\*_Latest overall cohort rate_ calculated as at latest date for vaccinations recorded across all TPP practices.**
-#
 
 # +
 chartlist = find_and_sort_filenames(foldername="figures", file_extension=IMAGE_FORMAT.extension)
@@ -123,15 +145,14 @@ chartlist = find_and_sort_filenames(foldername="figures", file_extension=IMAGE_F
 display(Markdown("## 80+ population"))
 for item in chartlist:
     show_chart(item, IMAGE_FORMAT)
-
 # -
 
-# ## Trends in vaccination rates of 70-79 population according to demographic/clinical features, cumulatively by day. <a name='charts70' />
+# # 
+# ## Trends in vaccination rates of **70-79** population according to demographic/clinical features, cumulatively by day. <a name='charts70' />
 # **\*National rate calculated as at latest date for vaccinations recorded across all TPP practices.**
-#
+#     
 
 # +
-    
 display(Markdown("## 70-79 population"))
 chartlist2 = find_and_sort_filenames(foldername="figures", population_subset="70-79", file_extension=IMAGE_FORMAT.extension)
     
@@ -142,9 +163,9 @@ for item in chartlist2:
 # ## 
 # ## Trends in vaccination rates of **shielding** population according to demographic/clinical features, cumulatively by day. <a name='charts_shield' />
 # **\*National rate calculated as at latest date for vaccinations recorded across all TPP practices.**
+#
 
 # +
-    
 display(Markdown("## Shielding population (aged 16-69)"))
 chartlist2 = find_and_sort_filenames(foldername="figures", population_subset="shielding (aged 16-69)", file_extension=IMAGE_FORMAT.extension)
     
@@ -157,7 +178,6 @@ for item in chartlist2:
 # **\*National rate calculated as at latest date for vaccinations recorded across all TPP practices.**
 
 # +
-    
 display(Markdown("## 65-69 population"))
 chartlist2 = find_and_sort_filenames(foldername="figures", population_subset="65-69", file_extension=IMAGE_FORMAT.extension)
     
@@ -168,6 +188,7 @@ for item in chartlist2:
 # ## 
 # ## Trends in vaccination rates of 60-64 population according to demographic/clinical features, cumulatively by day. <a name='charts60' />
 # **\*National rate calculated as at latest date for vaccinations recorded across all TPP practices.**
+#
 
 # +
 display(Markdown("## 60-64 population"))
@@ -192,10 +213,24 @@ for item in chartlist2:
 # ## 
 # ## Trends in vaccination rates of 50-54 population according to demographic/clinical features, cumulatively by day. <a name='charts50' />
 # **\*National rate calculated as at latest date for vaccinations recorded across all TPP practices.**
+#
+# +
 
 # +
 display(Markdown("## 50-54 population"))
 chartlist2 = find_and_sort_filenames(foldername="figures", population_subset="50-54", file_extension=IMAGE_FORMAT.extension)
+    
+for item in chartlist2:
+    show_chart(item, IMAGE_FORMAT)
+# -
+
+# ## 
+# ## Trends in vaccination rates of 40-49 population according to demographic/clinical features, cumulatively by day. <a name='charts40' />
+# **\*National rate calculated as at latest date for vaccinations recorded across all TPP practices.**
+
+# +
+display(Markdown("## 40-49 population"))
+chartlist2 = find_and_sort_filenames(foldername="figures", population_subset="40-49", file_extension=IMAGE_FORMAT.extension)
     
 for item in chartlist2:
     show_chart(item, IMAGE_FORMAT)
@@ -229,4 +264,4 @@ display(Markdown("- Ethnicity information is primarily retrieved from GP records
                  \n recorded in inpatients, outpatients or A&E over the last ~5 years (or latest if tied).\
                  \n- Patient counts are rounded to the nearest 7"))
 
-tab[["total population (n)","ethnicity coverage (%)"]].drop("vaccinated 16-49, not in other eligible groups shown")
+tab[["total population (n)","ethnicity coverage (%)"]]
